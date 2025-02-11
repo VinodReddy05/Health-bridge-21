1430253e68ca4c52c91b1f24ac83001058a1350f
Janagam Vinod Reddy <jvinodreddy308@gmail.com>
2025-02-11 21:00:57 +0530

    vite-config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Health-bridge-21/',  // Update with your GitHub repo name
+  build: {
+    outDir: 'build',  // Ensure this is set
+  },
   plugins: [
     react()
   ],
