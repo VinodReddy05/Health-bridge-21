72b1d13fc007d1e02f8a8a97c66f46dd869e7183
Janagam Vinod Reddy <jvinodreddy308@gmail.com>
2025-02-11 21:04:17 +0530

    Updates

---
+++ assets/ChecklistRtl-DqAmTbcq.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-CEwMMO6D.js";import{c as o}from"./createSvgIcon-ksZbkFwt.js";const c=o(t.jsx("path",{d:"M11 7H2v2h9zm0 8H2v2h9zm5.34-4L12.8 7.46l1.41-1.41 2.12 2.12 4.24-4.24L22 5.34zm0 8-3.54-3.54 1.41-1.41 2.12 2.12 4.24-4.24L22 13.34z"}),"ChecklistRtl");export{c as C};
