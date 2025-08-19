1e4f938b3b971d1bc1250fc1cdf4c931f0a7c53c
Janagam Vinod Reddy <jvinodreddy308@gmail.com>
2025-08-19 21:14:16 +0530

    Updates

---
+++ assets/Delete-BuKSSMoj.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-dd44u1t_.js";import{c as t}from"./createSvgIcon-oaL59T5h.js";const r=t(e.jsx("path",{d:"M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6zM19 4h-3.5l-1-1h-5l-1 1H5v2h14z"}),"Delete");export{r as D};
